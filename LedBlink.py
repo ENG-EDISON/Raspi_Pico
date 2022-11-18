c6271c986fe7a8d103e3ccf8db39143a4c110a70
Edison Ngunjiri <edisonngunjiri@gmail.com>
2022-11-18 19:41:13 +0300

    Code to blink Led

--- LedBlink.py
+++ LedBlink.py
@@ -1,9 +1,17 @@
 import machine
 import time
+#Use GPIO naming Convention.
 LED=2;
+#Create a Led object
+#set the Led as either INPUT or OUTPUT
 led = machine.Pin(LED, machine.Pin.OUT)
+#Forever running loop
 while True:
+    #Turn the led on
     led.off()
+    #Delay for 1 second
     time.sleep(1)
+    #Turn led off
     led.on()
+    #Delay for 1 second
     time.sleep(1)
