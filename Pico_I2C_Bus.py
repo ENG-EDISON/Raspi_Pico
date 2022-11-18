87206cd6f84fd10a238bf8b824689238e66628f6
Edison Ngunjiri <edisonngunjiri@gmail.com>
2022-11-18 20:59:10 +0300

    Pico I2C code

--- Pico_I2C_Bus.py
+++ Pico_I2C_Bus.py
@@ -1,13 +1,19 @@
 from machine import Pin, SoftI2C
-
+import time
+#create an I2C object
+#set SCL_PIN->GPIO5
+#set SDA_PIN->GPIO4
+#set frequency to 100kHz
 i2c = SoftI2C(scl=Pin(5), sda=Pin(4), freq=100_000)
 # scan for devices
-i2c.scan()              
-# read 4 bytes from device with address 0x3a
-i2c.readfrom(0x3a, 4)
-# write '12' to device with address 0x3a
- # create a buffer with 10 bytes
-i2c.writeto(0x3a, '12') 
-buf = bytearray(10)
- # write the given buffer to the peripheral
-i2c.writeto(0x3a, buf) 
+i2c.scan()
+while True:
+    # read 4 bytes from device with address 0x3a
+    i2c.readfrom(0x3a, 4)
+    # write '12' to device with address 0x3a
+    # create a buffer with 10 bytes
+    i2c.writeto(0x3a, '12')
+    buf = bytearray(10)
+    # write the given buffer to the peripheral
+    i2c.writeto(0x3a, buf)
+    time.sleep(1)
